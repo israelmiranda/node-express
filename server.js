17da417b7f51f8b37f0aaee8d4e5313ff815f07d
Israel Miranda <israelmmiranda@gmail.com>
2024-06-28 18:07:05 -0300

    ? - add dotenv

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
+import "dotenv/config";
 import app from './src/app.js';
 
-const PORT = 3000;
+const PORT = process.env.PORT;
 
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  console.log(`[server]: Server is running at http://localhost:${PORT}`);
 });
